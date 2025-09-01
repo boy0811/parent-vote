4862b8782b1f6b6e4b4e26cb4860858617e4678c
boy0811 <boy0811@gmail.com>
2025-09-01 20:33:27 +0800

    fix: expand admin.password_hash length and init admin support

--- models.py
+++ models.py
@@ -73,7 +73,7 @@ class Setting(db.Model):
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
